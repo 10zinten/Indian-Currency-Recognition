ca132e73db58980853ac83b4318b18f3f0a7cebb
tenzin <ten13zin@gmail.com>
2018-08-02 16:27:14 +0530

    start django project for interface

---
+++ app/icr/urls.py
@@ -0,0 +1,31 @@
+"""foodImageClassifier URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf import settings
+from django.conf.urls import url
+from django.conf.urls.static import static
+from django.contrib import admin
+from classifier.views import upload_img, predict, clean_up
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', upload_img, name='upload_img'),
+    url(r'^predict/$', predict, name='predict'),
+    url(r'^back/$', clean_up, name='clean_up')
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
